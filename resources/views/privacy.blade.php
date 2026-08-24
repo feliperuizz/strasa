47c25b31733f3a1d3fb4ea65c57a3279dd6a412c
Felipe Ruiz <felipepiliruiz@gmail.com>
2026-08-24 17:22:50 -0300

    fix: adiciona cache buster no favicon para forcar atualizacao

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Política de Privacidade - strasa</title>
-    <link rel="icon" type="image/png" href="{{ asset('strasafavicon2.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('strasafavicon2.png') }}?v={{ time() }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
